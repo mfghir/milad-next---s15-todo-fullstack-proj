e2e92f3eb2f1037e5c0c1579ef683e79fb9d61bd
mfghir <fatemeghafari77@gmail.com>
2023-06-03 00:46:54 -0700

    ep216 - layout

---
+++ components/layout/Layout.js
@@ -0,0 +1,38 @@
+import Link from "next/link";
+import { VscListSelection } from "react-icons/vsc";
+import { BiMessageSquareAdd } from "react-icons/bi";
+import { RxDashboard } from "react-icons/rx";
+
+const Layout = ({ children }) => {
+  return (
+    <div className="container">
+      <header>
+        <p>Fatemeweb Todo App</p>
+      </header>
+
+      <div className="container--main">
+        <aside>
+          <p>Welcome 👋</p>
+          <ul>
+            <li>
+              <VscListSelection />
+              <Link href="/">Todos</Link>
+            </li>
+            <li>
+              <BiMessageSquareAdd />
+              <Link href="/add-todo">Add Todo</Link>
+            </li>
+            <li>
+              <RxDashboard />
+              <Link href="/profile">Profile</Link>
+            </li>
+          </ul>
+        </aside>
+
+        <section>{children}</section>
+      </div>
+    </div>
+  );
+};
+
+export default Layout;
