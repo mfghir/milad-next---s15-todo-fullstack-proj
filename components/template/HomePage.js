2e5b5d6f7ca1f5a2600f8b20478c5b4478056419
mfghir <fatemeghafari77@gmail.com>
2023-06-05 03:36:39 -0700

    ep232

--- components/template/HomePage.js
+++ components/template/HomePage.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import Tasks from "../module/Tasks";
 
 const HomePage = () => {
   const [todos, setTodos] = useState([]);
@@ -17,18 +18,22 @@ const HomePage = () => {
     <div className="home-page">
       <div className="home-page--todo">
         <p>Todo</p>
+        <Tasks data={todos.todo} />
       </div>
 
       <div className="home-page--inProgress">
         <p>In Progress</p>
+        <Tasks data={todos.inProgress} />
       </div>
 
       <div className="home-page--review">
         <p>Review</p>
+        <Tasks data={todos.review} />
       </div>
 
       <div className="home-page--done">
         <p>Done</p>
+        <Tasks data={todos.done} />
       </div>
     </div>
   );
