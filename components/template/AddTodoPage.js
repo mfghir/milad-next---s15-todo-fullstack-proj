603410365203e6aed4f8b439833fab2098789609
mfghir <fatemeghafari77@gmail.com>
2023-06-05 01:13:12 -0700

    ep229

--- components/template/AddTodoPage.js
+++ components/template/AddTodoPage.js
@@ -7,10 +7,27 @@ import { MdDoneAll } from "react-icons/md";
 import { useState } from "react";
 
 import RadioButton from "../element/RadioButton";
+import "react-toastify/dist/ReactToastify.css";
+import { ToastContainer, toast } from "react-toastify";
 
 const AddTodoPage = () => {
   const [title, setTitle] = useState("");
   const [status, setStatus] = useState("todo");
+
+  const addHandler = async () => {
+    const res = await fetch("/api/todos", {
+      method: "POST",
+      body: JSON.stringify({ title, status }),
+      headers: { "Content-Type": "application/json" },
+    });
+
+    const data = await res.json();
+    if (data.status === "success") {
+      setTitle("");
+      setStatus("todo");
+      toast.success("Todo Added!");
+    }
+  };
 
   return (
     <div className="add-form">
@@ -68,8 +85,10 @@ const AddTodoPage = () => {
           </RadioButton>
         </div>
 
-        <button>Add</button>
+        <button onClick={addHandler}>Add</button>
       </div>
+
+      <ToastContainer />
     </div>
   );
 };
